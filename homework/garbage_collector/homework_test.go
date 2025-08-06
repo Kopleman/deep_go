e5d82a771f5698453958ff78b7ece36abd1537e1
Алексей Двуреченский <Aleksey.Dvurechenskiy@leroymerlin.ru>
2025-08-06 18:31:25 +0300

    feat(garbage_collector): basic implementation

--- homework/garbage_collector/homework_test.go
+++ homework/garbage_collector/homework_test.go
@@ -11,8 +11,34 @@ import (
 // go test -v homework_test.go
 
 func Trace(stacks [][]uintptr) []uintptr {
-	// need to implement
-	return nil
+	visited := make(map[*uintptr]bool)
+	var result []uintptr
+
+	for _, s := range stacks {
+		for _, ptr := range s {
+			memPtr := (*uintptr)(unsafe.Pointer(ptr))
+			if ptr != 0 && !visited[memPtr] {
+				visited[memPtr] = true
+				result = append(result, ptr)
+
+				processObject(ptr, visited, &result)
+			}
+		}
+	}
+
+	return result
+}
+
+func processObject(ptr uintptr, visited map[*uintptr]bool, result *[]uintptr) {
+	memPtr := (*uintptr)(unsafe.Pointer(ptr))
+	value := *memPtr
+	valueAsMemPointer := (*uintptr)(unsafe.Pointer(value))
+	if value != 0 && !visited[valueAsMemPointer] {
+		visited[valueAsMemPointer] = true
+		*result = append(*result, value)
+
+		processObject(value, visited, result)
+	}
 }
 
 func TestTrace(t *testing.T) {
@@ -47,8 +73,14 @@ func TestTrace(t *testing.T) {
 	}
 
 	pointers := Trace(stacks)
+	/**
+	[0]heapPointer1->[1]heapObjects[1]
+	и только потом мы дойдем до
+	[2]heapObjects[0]
+	*/
 	expectedPointers := []uintptr{
 		uintptr(unsafe.Pointer(&heapPointer1)),
+		uintptr(unsafe.Pointer(&heapObjects[1])),
 		uintptr(unsafe.Pointer(&heapObjects[0])),
 		uintptr(unsafe.Pointer(&heapPointer2)),
 		uintptr(unsafe.Pointer(&heapObjects[1])),
